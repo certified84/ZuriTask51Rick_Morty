7b256e0161b5e61b2e835f92e6e0f09d80f2b3ea
Samson Achiaga <sirhumble84@gmail.com>
2021-05-18 23:54:56 +0100

    Initial commit

---
+++ app/src/main/java/com/certified/zuritask51rickmorty/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.certified.zuritask51rickmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
